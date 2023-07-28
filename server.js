403277c3dcb752f7bc3526a802c3504adf30bead
Marjan-MN <131532495+Marjan-MN@users.noreply.github.com>
2023-07-27 23:15:53 -0500

    fixed the schema file

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 const db = mysql.createConnection(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PASSWORD
 )
 
 // Query database
